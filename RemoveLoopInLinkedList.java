3eb1c4c6abad3d67b6c9297da897753cab4c227e
ajt-rnk <ajeetvil@gmail.com>
2018-02-15 22:15:34 +0530

    Create RemoveLoopInLinkedList.java

---
+++ RemoveLoopInLinkedList.java
@@ -0,0 +1,29 @@
+/*
+Author:  Ajeet Kumar
+         15/02/2018
+Problem: 
+*/
+	  int removeTheLoop(Node node) {
+      Node low=node,high=node;
+      int result=0;
+      while(high.next!=null){
+          low=low.next;
+          high=high.next;
+          if(high.next!=null)
+          high=high.next;
+          if(low.data==high.data){
+              result=1;
+              break;
+          }
+      }
+      low=node;
+      while(result==1){
+          if(high.next.data==low.next.data){
+              high.next=null;
+              break;
+          }
+          low=low.next;
+          high=high.next;
+      }
+      return result;
+}
